c9f893badac43dc680bbc94469e51a05b70d487b
wjhunter5315 <wjhunter5315@gmail.com>
2019-10-19 20:43:38 -0700

    big script push, for loop and conditionals

--- script.js
+++ script.js
@@ -1,46 +1,97 @@
 console.log(window);
-var passwordEl = document.querySelector("#password");
-var generateEl = document.querySelector("#generate");
-var copyEl = document.querySelector("#copy");
-
-
-function getPassword() {
+var password = "";
+var charOptions = [
+    ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", ",", ".", "/", ";", "'", "[", "]", "<", ">", "?", ":", "{", "}"],
+    ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+    ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+]
+function makePass(password) {
+    var passLength = prompt("Between 8 - 128, how long would you like your password?");
+    console.log(passLength);
     
-}
+    var specChar = confirm("Use special characters (*@#%!) ?");
+    console.log(specChar);
 
-function getLength() {
-    event.preventDefault();
-    var passLength = prompt("Between 8 - 128, how long would you like your password to be?");
-    for (var pLength = passLength.querySelector);
+    var caseUpper = confirm("Use Upper-case letters?");
+    console.log(caseUpper);
 
-}
+    var caseLower = confirm("Use Lower-case letters?");
+    console.log(caseLower);
+    
+    for(var i = 0; i < passLength; i++){
+        password += passLength.charAt(Math.floor(Math.random() * passLength.length));
+        
+        if (specChar === true && caseUpper === true && caseLower === true) {
+            var randomNumber1 = Math.floor(Math.random() * charOptions.length);
+            var randomNumber2 = Math.floor(Math.random() * charOptions[randomNumber1].length);
+            var randomEl = charOptions[randomNumber1][randomNumber2];
+            console.log(randomNumber1);
+            console.log(randomNumber2);
+            console.log(randomEl);
+        }
+       
+        else if (specChar === false && caseUpper === true && caseLower === true) {
+            var newArray = [0,2,3];
+            var randomNumber1 = newArray[Math.floor(Math.random() * newArray.length)];
+            var randomNumber2 = Math.floor(Math.random() * charOptions[randomNumber1].length);
+            var randomEl = charOptions[randomNumber1][randomNumber2];
+            console.log(randomNumber1);
+            console.log(randomEl);
+        }
 
-function getSpec() {
-    event.preventDefault();
-    var passSpec = confrim("Would you like your password to include special characters (&%!#^) ?");
-    if {
-        //[!@#$%^&*()_+-=,./;'[]\<>?:"{}|]
+        else if (specChar === false && caseUpper === false && caseLower === true) {
+            var newArray = [0,3];
+            var randomNumber1 = newArray[Math.floor(Math.random() * newArray.length)];
+            var randomNumber2 = Math.floor(Math.random() * charOptions[randomNumber1].length);
+            var randomEl = charOptions[randomNumber1][randomNumber2];
+            console.log(randomNumber1);
+            console.log(randomEl);
+        }
+
+        else if (specChar === false && caseUpper === true && caseLower === false) {
+            var newArray = [0,2];
+            var randomNumber1 = newArray[Math.floor(Math.random() * newArray.length)];
+            var randomNumber2 = Math.floor(Math.random() * charOptions[randomNumber1].length);
+            var randomEl = charOptions[randomNumber1][randomNumber2];
+            console.log(randomNumber1);
+            console.log(randomEl);
+        }
+
+        else if (specChar === true && caseUpper === true && caseLower === false) {
+            var newArray = [0,1,2];
+            var randomNumber1 = newArray[Math.floor(Math.random() * newArray.length)];
+            var randomNumber2 = Math.floor(Math.random() * charOptions[randomNumber1].length);
+            var randomEl = charOptions[randomNumber1][randomNumber2];
+            console.log(randomNumber1);
+            console.log(randomEl);
+        }
+
+        else {
+            var newArray = [0];
+            var randomNumber1 = Math.floor(Math.random() * newArray.length);
+            var randomNumber2 = Math.floor(Math.random() * charOptions[randomNumber1].length);
+            var randomEl = charOptions[randomNumber1][randomNumber2];
+            console.log(randomNumber1);
+            console.log(randomEl);
+        }
     }
 }
+console.log(password);
 
-function getUcase() {
-    event.preventDefault();
-    var passUcase = confrim("Would you like your password to have upper-case letters?");
-    if {
-        //[ABCDEFGHIJKLMNOPQRSTUVWXYZ]
-    }
-}
 
-function getLcase() {
-    event.preventDefault();
-    var passLcase = confirm("Would you like your password to have lower-case letters?");
-    var lCase = []
-    if {
-        //[abcdefghijklmnopqrstuvwxyz]
-    }
-}
 
-password.getLength();
-password.getSpec();
-password.getUcase();
-password.getLcase();
+makePass();
+
+//getSpec();
+//getUcase();
+//getLcase();
+//console.log(window);
+//console.log(lengthEl);
+
+
+//var randomNumber1 = Math.floor(Math.random() * charOptions.length);
+//var randomNumber2 = Math.floor(Math.random() * charOptions[randomNumber1].length);
+//console.log(randomNumber1);
+//console.log(randomNumber2);
+//console.log(charOptions[randomNumber1][randomNumber2]);
