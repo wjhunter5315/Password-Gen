88e9abf996396dc8cb2007524f07320b4298052f
wjhunter5315 <wjhunter5315@gmail.com>
2019-10-22 16:30:02 -0700

    pass-gen works, needs copy button and num valid

--- script.js
+++ script.js
@@ -1,107 +1,143 @@
 console.log(window);
-var password = "";
+var password = [];
 var passEl = [];
+var passString = [];
+
 var charOptions = [
     ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
     ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", ",", ".", "/", ";", "'", "[", "]", "<", ">", "?", ":", "{", "}"],
     ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
     ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 ]
-function makePass(password) {
-    var passLength = prompt("Between 8 - 128, how long would you like your password?");
-    console.log(passLength);
-    
-    var specChar = confirm("Use special characters (*@#%!) ?");
-    console.log(specChar);
-
-    var caseUpper = confirm("Use Upper-case letters?");
-    console.log(caseUpper);
-
-    var caseLower = confirm("Use Lower-case letters?");
-    console.log(caseLower);
-    
-    for(var i = 0; i < passLength; i++){
-        password += passLength.charAt(Math.floor(Math.random() * passLength.length));
+document.getElementById("generate").addEventListener("click", makePass);
+    function makePass() {
+        var passLength = prompt("Between 8 - 128, how long would you like your password?");
+        console.log(passLength);
         
-        if (specChar === true && caseUpper === true && caseLower === true) {
-            var randomNumber1 = Math.floor(Math.random() * charOptions.length);
-            var randomNumber2 = Math.floor(Math.random() * charOptions[randomNumber1].length);
-            var randomEl = charOptions[randomNumber1][randomNumber2];
-            console.log(randomNumber1);
-            console.log(randomNumber2);
-            console.log(randomEl);
+        var specChar = confirm("Use special characters (*@#%!) ?");
+        console.log(specChar);
+    
+        var caseUpper = confirm("Use Upper-case letters?");
+        console.log(caseUpper);
+    
+        var caseLower = confirm("Use Lower-case letters?");
+        console.log(caseLower);
+        
+        for(var i = 0; i < passLength; i++){
+            password += passLength.charAt(Math.floor(Math.random() * passLength.length));
             
-            passEl.push(randomEl);
-            console.log(passEl);
-            
+            if (specChar === true && caseUpper === true && caseLower === true) {
+                var randomNumber1 = Math.floor(Math.random() * charOptions.length);
+                var randomNumber2 = Math.floor(Math.random() * charOptions[randomNumber1].length);
+                var randomEl = charOptions[randomNumber1][randomNumber2];
+                console.log(randomNumber1);
+                console.log(randomNumber2);
+                console.log(randomEl);
+                
+                passEl.push(randomEl);
+                console.log(passEl.join(""));
+                
+            }
+           
+            else if (specChar === false && caseUpper === true && caseLower === true) {
+                var newArray = [0,2,3];
+                var randomNumber1 = newArray[Math.floor(Math.random() * newArray.length)];
+                var randomNumber2 = Math.floor(Math.random() * charOptions[randomNumber1].length);
+                var randomEl = charOptions[randomNumber1][randomNumber2];
+                console.log(randomNumber1);
+                console.log(randomEl);
+                passEl.push(randomEl);
+                console.log(passEl);
+            }
+    
+            else if (specChar === false && caseUpper === false && caseLower === true) {
+                var newArray = [0,3];
+                var randomNumber1 = newArray[Math.floor(Math.random() * newArray.length)];
+                var randomNumber2 = Math.floor(Math.random() * charOptions[randomNumber1].length);
+                var randomEl = charOptions[randomNumber1][randomNumber2];
+                console.log(randomNumber1);
+                console.log(randomEl);
+                passEl.push(randomEl);
+                console.log(passEl);
+            }
+    
+            else if (specChar === false && caseUpper === true && caseLower === false) {
+                var newArray = [0,2];
+                var randomNumber1 = newArray[Math.floor(Math.random() * newArray.length)];
+                var randomNumber2 = Math.floor(Math.random() * charOptions[randomNumber1].length);
+                var randomEl = charOptions[randomNumber1][randomNumber2];
+                console.log(randomNumber1);
+                console.log(randomEl);
+                passEl.push(randomEl);
+                console.log(passEl);
+            }
+    
+            else if (specChar === true && caseUpper === true && caseLower === false) {
+                var newArray = [0,1,2];
+                var randomNumber1 = newArray[Math.floor(Math.random() * newArray.length)];
+                var randomNumber2 = Math.floor(Math.random() * charOptions[randomNumber1].length);
+                var randomEl = charOptions[randomNumber1][randomNumber2];
+                console.log(randomNumber1);
+                console.log(randomEl);
+                passEl.push(randomEl);
+                console.log(passEl);
+            }
+            else if (specChar === true && caseUpper === false && caseLower === false) {
+                var newArray = [0,1];
+                var randomNumber1 = newArray[Math.floor(Math.random() * newArray.length)];
+                var randomNumber2 = Math.floor(Math.random() * charOptions[randomNumber1].length);
+                var randomEl = charOptions[randomNumber1][randomNumber2];
+                console.log(randomNumber1);
+                console.log(randomEl);
+                passEl.push(randomEl);
+                console.log(passEl);
+            }
+            else {
+                var newArray = [0];
+                var randomNumber1 = Math.floor(Math.random() * newArray.length);
+                var randomNumber2 = Math.floor(Math.random() * charOptions[randomNumber1].length);
+                var randomEl = charOptions[randomNumber1][randomNumber2];
+                console.log(randomNumber1);
+                console.log(randomEl);
+                passEl.push(randomEl);
+                console.log(passEl);
+            }
         }
+        for (var i = 0; i < passEl; i++) {
+            password.push(passEl);
+            console.log(password);
+            var passString = password.join("");
+            console.log(passString);
+        }
+        
+        // makeString();
+        // var myJSON = JSON.stringify(passEl);
+        // console.log(myJSON);
+        // document.getElementById("password").value = myJSON;
+        document.getElementById("password").value = passEl.join("");
        
-        else if (specChar === false && caseUpper === true && caseLower === true) {
-            var newArray = [0,2,3];
-            var randomNumber1 = newArray[Math.floor(Math.random() * newArray.length)];
-            var randomNumber2 = Math.floor(Math.random() * charOptions[randomNumber1].length);
-            var randomEl = charOptions[randomNumber1][randomNumber2];
-            console.log(randomNumber1);
-            console.log(randomEl);
-        }
-
-        else if (specChar === false && caseUpper === false && caseLower === true) {
-            var newArray = [0,3];
-            var randomNumber1 = newArray[Math.floor(Math.random() * newArray.length)];
-            var randomNumber2 = Math.floor(Math.random() * charOptions[randomNumber1].length);
-            var randomEl = charOptions[randomNumber1][randomNumber2];
-            console.log(randomNumber1);
-            console.log(randomEl);
-        }
-
-        else if (specChar === false && caseUpper === true && caseLower === false) {
-            var newArray = [0,2];
-            var randomNumber1 = newArray[Math.floor(Math.random() * newArray.length)];
-            var randomNumber2 = Math.floor(Math.random() * charOptions[randomNumber1].length);
-            var randomEl = charOptions[randomNumber1][randomNumber2];
-            console.log(randomNumber1);
-            console.log(randomEl);
-        }
-
-        else if (specChar === true && caseUpper === true && caseLower === false) {
-            var newArray = [0,1,2];
-            var randomNumber1 = newArray[Math.floor(Math.random() * newArray.length)];
-            var randomNumber2 = Math.floor(Math.random() * charOptions[randomNumber1].length);
-            var randomEl = charOptions[randomNumber1][randomNumber2];
-            console.log(randomNumber1);
-            console.log(randomEl);
-        }
-
-        else {
-            var newArray = [0];
-            var randomNumber1 = Math.floor(Math.random() * newArray.length);
-            var randomNumber2 = Math.floor(Math.random() * charOptions[randomNumber1].length);
-            var randomEl = charOptions[randomNumber1][randomNumber2];
-            console.log(randomNumber1);
-            console.log(randomEl);
-        }
     }
-    var myJSON = JSON.stringify(passEl);
-    console.log(myJSON);
-}
-document.getElementById("generate").addEventListener("click", function(){
-    passEl.push(textarea);
-})
+
+    // passEl.push("password");
+
+// document.getElementById("generate").addEventListener("click", makePass()) {
+    // passEl.push("password");
+
 console.log(password);
 
 
 
-makePass();
+// makePass();
 
-//getSpec();
-//getUcase();
-//getLcase();
-//console.log(window);
-//console.log(lengthEl);
+// getSpec();
+// getUcase();
+// getLcase();
+// console.log(window);
+// console.log(lengthEl);
 
 
-//var randomNumber1 = Math.floor(Math.random() * charOptions.length);
-//var randomNumber2 = Math.floor(Math.random() * charOptions[randomNumber1].length);
-//console.log(randomNumber1);
-//console.log(randomNumber2);
-//console.log(charOptions[randomNumber1][randomNumber2]);
+// var randomNumber1 = Math.floor(Math.random() * charOptions.length);
+// var randomNumber2 = Math.floor(Math.random() * charOptions[randomNumber1].length);
+// console.log(randomNumber1);
+// console.log(randomNumber2);
+// console.log(charOptions[randomNumber1][randomNumber2]);
